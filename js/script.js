b1cccb50b8817fee3a6c65d12d756af7a03b6b8b
Garrett Barnard <garrettjbarnard@gmail.com>
2022-06-23 22:42:07 -0700

    pushing final touches on magic 8 ball final

--- js/script.js
+++ js/script.js
@@ -1,32 +1,61 @@
-// const cycle = document.getElementById("magic8Ball");
-// cycle.addEventListener("mouseover", cycleFunction);
+// START BUTTON ACTIONS
 
-// function cycleFunction(){
-//     let i = 0;
-    
-//     while (i < 1000){
-    
-//         console.log(i);
-//         i++;
+const start = document.getElementById("start");
+start.addEventListener("click", startFunction);
 
-//     }
-// }
+function startFunction(){
+   
+    document.querySelector("input").style.display = "initial";
+    document.querySelector(".button").style.display = "initial";
+    document.querySelector("p").style.display = "flex";
+    document.querySelector(".start").style.display = "none";
+
+}
+
+// SUBMIT BUTTON ACTIONS
 
 const click = document.getElementById("button");
 click.addEventListener("click", randomFunction);
 
 function randomFunction(){
-   const newImg = Math.ceil(Math.random() * 20);
-   let newSrc = `./media/magic8ball_${newImg}.png`
-    document.getElementById("magic8Ball").src = newSrc;
-    document.querySelector("input").style.display = "none";
-    document.querySelector(".reset").style.display = "initial";
-    document.querySelector(".button").style.display = "none";
+   
+
+   
+    const output = document.getElementById("ptag")
+    let input = document.forms["contact"]["question"].value;
+
+    if(input.length === 0){
+        document.querySelector("p").innerHTML = "YOU NEED TO ASK A QUESTION!!!"
+        document.querySelector(".black").style.opacity = "1";
+        document.getElementById("input").placeholder = "Dont.......dont anger the orb...";
+        document.getElementById("magic8BallStart").src = `./media/magic8ball_anger.png`;
+        document.getElementById("magic8Ball").src = `./media/magic8ball_anger.png`;
+    } else {
+        const newImg = Math.ceil(Math.random() * 20);
+        let newSrc = `./media/magic8ball_${newImg}.png`
+        document.getElementById("magic8Ball").src = newSrc;
+        document.querySelector("input").style.display = "none";
+        document.querySelector(".reset").style.display = "initial";
+        document.querySelector(".button").style.display = "none";
+        document.querySelector("p").innerHTML = input;
+        document.querySelector(".black").style.opacity = "0";
+        document.getElementById("input").placeholder = "What is your question?";
+        document.getElementById("magic8BallStart").src = `./media/magic8ball_start.png`;
+    }
+
+    
 }
 
-const output = document.getElementById("ptag")
+// RESET BUTTON ACTIONS
 
-document.forms.contact.addEventListener('button', function(event){
-    console.log("test");
-    output.innerHTML = (`Name: ${event.target.Name.value} `);
-    });
+const onPress = document.getElementById("reset");
+onPress.addEventListener("click", resetFunction);
+
+function resetFunction(){
+    document.querySelector("input").style.display = "initial";
+    document.querySelector(".reset").style.display = "none";
+    document.querySelector(".button").style.display = "initial";
+    document.querySelector("p").innerHTML = "Ask your question and submit it to THE ORB.";
+    document.getElementById("magic8Ball").src = `./media/magic8ball_start.png`;
+}
+
